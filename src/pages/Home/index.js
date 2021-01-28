011c22dea0a776debd5e1d161167ecbff15b392f
victor-pagnozi <victor.pagnozi@gmail.com>
2021-01-28 17:18:43 -0300

    Dado continuação na montagem do form para cadastrar ocorrência

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import './styles.css';
 import {
   Button,
   Form,
@@ -17,77 +18,66 @@ const Home = (props) => {
       <Form>
         <Row form>
           <Col md={8}>
-            <FormGroup>
-              <Label for="servico">Serviço a ser verificado</Label>
-              <Input type="select" name="servico" id="servico">
-                <option>Rompimento</option>
-                <option>Acompanhamento de energia</option>
-                <option>Lançamento de Cordoalha</option>
-                <option>Lançamento de Cabo</option>
-                <option>Montagem de CEO</option>
-                <option>Montagem de CTO</option>
-                <option>Outros</option>
-              </Input>
-            </FormGroup>
+            <Label for="servico">Serviço a ser verificado</Label>
+            <Input type="select" name="servico" id="servico">
+              <option>Rompimento</option>
+              <option>Acompanhamento de energia</option>
+              <option>Lançamento de Cordoalha</option>
+              <option>Lançamento de Cabo</option>
+              <option>Montagem de CEO</option>
+              <option>Montagem de CTO</option>
+              <option>Outros</option>
+            </Input>
           </Col>
         </Row>
 
         <Row form>
           <Col md={8}>
-            <FormGroup>
-              <Label for="logradouro-servico">Local do servico</Label>
-              <Input
-                type="text"
-                name="logradouro-servico"
-                id="logradouro-servico"
-                placeholder="Inisira aqui qual o endereço do serviço a ser executado"
-              />
-            </FormGroup>
+            <Label for="logradouro-servico">Local do servico</Label>
+            <Input
+              type="text"
+              name="logradouro-servico"
+              id="logradouro-servico"
+              placeholder="Inisira aqui qual o endereço do serviço a ser executado"
+            />
           </Col>
         </Row>
 
-        <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            id="examplePassword"
-            placeholder="password placeholder"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleText">Text Area</Label>
-          <Input type="textarea" name="text" id="exampleText" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleFile">File</Label>
-          <Input type="file" name="file" id="exampleFile" />
-          <FormText color="muted">
-            This is some placeholder block-level help text for the above input.
-            It's a bit lighter and easily wraps to a new line.
-          </FormText>
-        </FormGroup>
-        <FormGroup tag="fieldset">
-          <legend>Radio Buttons</legend>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Option one is this and
-              that—be sure to include why it's great
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Option two can be something
-              else and selecting it will deselect option one
-            </Label>
-          </FormGroup>
-          <FormGroup check disabled>
-            <Label check>
-              <Input type="radio" name="radio1" disabled /> Option three is
-              disabled
-            </Label>
-          </FormGroup>
-        </FormGroup>
+        <Row form>
+          <Col md={8}>
+            <Label for="descricao-servico">Descrição do Serviço</Label>
+            <Input
+              type="textarea"
+              name="text"
+              id="descricao-servico"
+              row="10"
+            />
+          </Col>
+        </Row>
+
+        <Row form>
+          <Col md={8}>
+            <Label for="anexo">Anexo</Label>
+            <Input type="file" name="anexo" id="anexo" />
+            <FormText color="muted">
+              Insira aqui um anexo para facilitar o entendimento do serviço a
+              ser realizado
+            </FormText>
+          </Col>
+        </Row>
+
+        <Row form>
+          <Col md={8}>
+            <Label for="equipe">Equipe</Label>
+            <Input type="select" name="equipe" id="equipe">
+              <option>Equipe Fulano</option>
+              <option>Equipe ciclano</option>
+              <option>Equipe beltrano</option>
+              <option>Outros</option>
+            </Input>
+          </Col>
+        </Row>
+
         <FormGroup check>
           <Label check>
             <Input type="checkbox" /> Check me out
